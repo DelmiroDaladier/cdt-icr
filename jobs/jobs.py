86a9c70a5cf98a696aca41229264cef44109ac54
DelmiroDaladier <daladiersampaio@gmail.com>
2023-08-25 10:03:05 +0100

    changes: python code formatting for jobs folder

--- jobs/jobs.py
+++ jobs/jobs.py
@@ -21,38 +21,32 @@ def schedule_api():
         None
     """
     newsletter_objects = Newsletter.objects.all()
-    latest_newsletter = newsletter_objects.latest('modified_at')
+    latest_newsletter = newsletter_objects.latest("modified_at")
 
     latest_date = latest_newsletter.modified_at
 
     posts = Publication.objects.filter(
-        updated_at__range=(
-            latest_date,
-            datetime.now()))
+        updated_at__range=(latest_date, datetime.now())
+    )
     posts = list(posts.values())
 
     conferences = Conference.objects.filter(
-        updated_at__range=(latest_date, datetime.now()))
+        updated_at__range=(latest_date, datetime.now())
+    )
     conferences = list(conferences.values())
 
-    content = {
-        'posts': posts,
-        'conferences': conferences
-    }
+    content = {"posts": posts, "conferences": conferences}
 
     current_path = os.getcwd()
-    filepath = current_path + \
-        f'/newsletter_frontend/index.qmd'
+    filepath = current_path + f"/newsletter_frontend/index.qmd"
 
     create_qmd_file(filepath)
 
     generate_page_content(filepath, content)
 
-    repo = 'newsletter_frontend'
-    path = 'index.qmd'
-    print('creating push request')
+    repo = "newsletter_frontend"
+    path = "index.qmd"
+    print("creating push request")
     create_push_request(
-        file_path=filepath,
-        folder_name='',
-        repo=repo,
-        path=path)
+        file_path=filepath, folder_name="", repo=repo, path=path
+    )
--- jobs/updater.py
+++ jobs/updater.py
@@ -5,11 +5,10 @@ from .jobs import schedule_api
 
 def start():
     """
-Schedule a recurring job to call `schedule_api()` function every week, starting from March 15th, 2023.
+    Schedule a recurring job to call `schedule_api()` function every week, starting from March 15th, 2023.
 
-:return: None
-"""
-    start_date = datetime.strptime('2023-03-15', '%Y-%m-%d')
+    :return: None"""
+    start_date = datetime.strptime("2023-03-15", "%Y-%m-%d")
     scheduler = BackgroundScheduler()
-    scheduler.add_job(schedule_api, 'interval', start_date=start_date, weeks=1)
+    scheduler.add_job(schedule_api, "interval", start_date=start_date, weeks=1)
     scheduler.start()
