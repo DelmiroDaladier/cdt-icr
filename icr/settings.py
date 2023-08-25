b4e1af3859f6bbe23e6034e6357f13bfa2df4d08
DelmiroDaladier <daladiersampaio@gmail.com>
2023-08-25 10:02:19 +0100

    changes: python code formatting for files in icr folder

--- icr/settings.py
+++ icr/settings.py
@@ -16,12 +16,12 @@ from django.contrib.messages import constants as messages
 
 
 MESSAGE_TAGS = {
-        messages.DEBUG: 'alert-secondary',
-        messages.INFO: 'alert-info',
-        messages.SUCCESS: 'alert-success',
-        messages.WARNING: 'alert-warning',
-        messages.ERROR: 'alert-danger',
- }
+    messages.DEBUG: "alert-secondary",
+    messages.INFO: "alert-info",
+    messages.SUCCESS: "alert-success",
+    messages.WARNING: "alert-warning",
+    messages.ERROR: "alert-danger",
+}
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,71 +31,73 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)#qz*os981-@6@$uoo98%+3d4x$j6&1c%wjtx*)p4m$gyw06ad'
+SECRET_KEY = (
+    "django-insecure-)#qz*os981-@6@$uoo98%+3d4x$j6&1c%wjtx*)p4m$gyw06ad"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'repository',
-    'cdt_newsletter',
-    'blog',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'crispy_forms',
-    'modal_forms',
-    'formtools'
+    "repository",
+    "cdt_newsletter",
+    "blog",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "crispy_forms",
+    "modal_forms",
+    "formtools",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'icr.urls'
+ROOT_URLCONF = "icr.urls"
 
 SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(SETTINGS_PATH, 'templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(SETTINGS_PATH, "templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'icr.wsgi.application'
+WSGI_APPLICATION = "icr.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
@@ -105,16 +107,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -122,9 +124,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -136,25 +138,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, 'icr/static/')
-]
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "icr/static/")]
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = "/"
 
-CSRF_TRUSTED_ORIGINS = ['https://cdt-icr.onrender.com']
+CSRF_TRUSTED_ORIGINS = ["https://cdt-icr.onrender.com"]
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'daladiersampaio@gmail.com' #Enter your email
-EMAIL_HOST_PASSWORD = 'lvwadclvplrrsdwk' #Enter the password to the email
+EMAIL_HOST_USER = "daladiersampaio@gmail.com"  # Enter your email
+EMAIL_HOST_PASSWORD = "lvwadclvplrrsdwk"  # Enter the password to the email
--- icr/urls.py
+++ icr/urls.py
@@ -1,42 +1,93 @@
-from django.conf import settings 
+from django.conf import settings
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 from django import views as django_views
 
 from blog.views import blog_homepage
-from cdt_newsletter.views import review_newsletter, create_newsletter, download_newsletter, create_announcement, NewsletterPreview, newsletter_submission_success, announcements, edit_announcement, announcement_detail, delete_announcement
+from cdt_newsletter.views import (
+    review_newsletter,
+    create_newsletter,
+    download_newsletter,
+    create_announcement,
+    NewsletterPreview,
+    newsletter_submission_success,
+    announcements,
+    edit_announcement,
+    announcement_detail,
+    delete_announcement,
+)
 from cdt_newsletter.forms import Newsletterform
-from repository.views import homepage, about, help_page, author_create, add_category, add_venue, update_post, arxiv_post, register_request, submit_conference, submit_session
+from repository.views import (
+    homepage,
+    about,
+    help_page,
+    author_create,
+    add_category,
+    add_venue,
+    update_post,
+    arxiv_post,
+    register_request,
+    submit_conference,
+    submit_session,
+)
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', homepage, name = 'homepage'),
-    path('about/', about, name = 'about'),
-    path('update_post/<slug>/', update_post, name='update_post'),
-    path('author_create/', author_create, name='author_create'),
-    path('add_category/', add_category, name='add_category'),
-    path('add_venue/', add_venue, name='add_venue'),
-    path('blog/', blog_homepage, name='blog_homepage'),
-    path('arxiv_post/', arxiv_post, name='arxiv_post'),
-    path('register/', register_request, name='register'),
-    path('submit_conference/', submit_conference, name='submit_conference'),
-    path('submit_session/', submit_session, name='submit_session'),
-    path('review_newsletter', review_newsletter, name='review_newsletter'),
-    path('create_newsletter', create_newsletter, name='create_newsletter'),
-    path('download_newsletter', download_newsletter, name='download_newsletter'),
-    path('create_announcement', create_announcement, name='create_announcement'),
-    path('newsletter_preview', NewsletterPreview(Newsletterform), name='newsletter_preview'),
-    path('newsletter_submission_success', newsletter_submission_success, name='newsletter_submission_success'),
-    path('announcements', announcements, name='announcements'),
-    path('announcement/<int:pk>', announcement_detail, name='announcement_detail'),
-    path('edit_announcement/<int:pk>/', edit_announcement, name='edit_announcement'),
-    path('delete_announcement/<int:pk>/', delete_announcement, name='delete_announcement'),
-    path('help', help_page, name='help'),
-    url(r'^jsi18n/$', django_views.i18n.JavaScriptCatalog.as_view(), name='jsi18n'),
+    path("admin/", admin.site.urls),
+    path("", homepage, name="homepage"),
+    path("about/", about, name="about"),
+    path("update_post/<slug>/", update_post, name="update_post"),
+    path("author_create/", author_create, name="author_create"),
+    path("add_category/", add_category, name="add_category"),
+    path("add_venue/", add_venue, name="add_venue"),
+    path("blog/", blog_homepage, name="blog_homepage"),
+    path("arxiv_post/", arxiv_post, name="arxiv_post"),
+    path("register/", register_request, name="register"),
+    path("submit_conference/", submit_conference, name="submit_conference"),
+    path("submit_session/", submit_session, name="submit_session"),
+    path("review_newsletter", review_newsletter, name="review_newsletter"),
+    path("create_newsletter", create_newsletter, name="create_newsletter"),
+    path(
+        "download_newsletter", download_newsletter, name="download_newsletter"
+    ),
+    path(
+        "create_announcement", create_announcement, name="create_announcement"
+    ),
+    path(
+        "newsletter_preview",
+        NewsletterPreview(Newsletterform),
+        name="newsletter_preview",
+    ),
+    path(
+        "newsletter_submission_success",
+        newsletter_submission_success,
+        name="newsletter_submission_success",
+    ),
+    path("announcements", announcements, name="announcements"),
+    path(
+        "announcement/<int:pk>",
+        announcement_detail,
+        name="announcement_detail",
+    ),
+    path(
+        "edit_announcement/<int:pk>/",
+        edit_announcement,
+        name="edit_announcement",
+    ),
+    path(
+        "delete_announcement/<int:pk>/",
+        delete_announcement,
+        name="delete_announcement",
+    ),
+    path("help", help_page, name="help"),
+    url(
+        r"^jsi18n/$",
+        django_views.i18n.JavaScriptCatalog.as_view(),
+        name="jsi18n",
+    ),
 ]
 
 urlpatterns += [
-    path('accounts/', include('django.contrib.auth.urls')),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
