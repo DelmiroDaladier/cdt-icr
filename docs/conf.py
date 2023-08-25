d0b9dd8aec3b9d43f2aa1e57a1458f94d58fd4ae
DelmiroDaladier <daladiersampaio@gmail.com>
2023-08-25 10:01:18 +0100

    changes: python code formating for python files in docs folder

--- docs/conf.py
+++ docs/conf.py
@@ -9,31 +9,29 @@ import os
 import sys
 import django
 
-sys.path.insert(0, os.path.abspath('..'))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'icr.settings'
+sys.path.insert(0, os.path.abspath(".."))
+os.environ["DJANGO_SETTINGS_MODULE"] = "icr.settings"
 django.setup()
 
 
-project = 'Interactive Content Repository'
-copyright = '2023, Daladier Sampaio'
-author = 'Daladier Sampaio'
-release = '1.0'
+project = "Interactive Content Repository"
+copyright = "2023, Daladier Sampaio"
+author = "Daladier Sampaio"
+release = "1.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 
-extensions = ['sphinx.ext.autodoc', 'sphinx_rtd_theme']
+extensions = ["sphinx.ext.autodoc", "sphinx_rtd_theme"]
 html_theme = "sphinx_rtd_theme"
 
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
